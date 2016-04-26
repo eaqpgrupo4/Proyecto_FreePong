2abbd47944e61eefc9e03c22f9f1387fb1eac7f8
raullorenzo <raul.lorenzo.67@gmail.com>
2016-04-26 20:45:50 +0200

    upload ionic

--- FreePong ionic/www/js/routes.js
+++ FreePong ionic/www/js/routes.js
@@ -14,11 +14,43 @@ angular.module('freepong.routes', [])
       templateUrl: 'templates/menu.html',
       controller: 'AppCtrl'
     })
-    .state('freepong.search', {
-      url: '/search',
+    .state('freepong.notificaciones', {
+      url: '/notificaciones',
       views: {
         'menuContent': {
-         templateUrl: 'templates/search.html'
+         templateUrl: 'templates/notificaciones.html'
+        }
+      }
+    })
+    .state('freepong.amigos', {
+      url: '/amigos',
+      views: {
+        'menuContent': {
+         templateUrl: 'templates/amigos.html'
+        }
+      }
+    })
+    .state('freepong.registro', {
+      url: '/registro',
+      views: {
+        'menuContent': {
+         templateUrl: 'templates/registro.html'
+        }
+      }
+    })
+    .state('freepong.login', {
+      url: '/login',
+      views: {
+        'menuContent': {
+         templateUrl: 'templates/login.html'
+        }
+      }
+    })
+    .state('freepong.logout', {
+      url: '/logout',
+      views: {
+        'menuContent': {
+         templateUrl: 'templates/logout.html'
         }
       }
     })
